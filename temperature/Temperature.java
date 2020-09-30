60726a9ff8af7d2ad6286d3985b8d160444aa44f
lightkun10 <lifeisubw@gmail.com>
2020-09-30 16:44:25 +0700

    Edit some dirty code

--- temperature/Temperature.java
+++ temperature/Temperature.java
@@ -11,14 +11,8 @@ public class Temperature {
 
         for (int i = 0; i < n; i++) {
             int t = in.nextInt(); // a temperature expressed as an integer ranging from -273 to 5526
-            //System.out.print(t + " ");
-            //if (t < 0) { t = (t - t) - t; }
             temperatures[i] = t;
         }
-
-        // Write an answer using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
 
         if (temperatures.length <= 0) {
             System.out.print("0");
@@ -43,7 +37,4 @@ public class Temperature {
         System.out.println(closer);
         
     }
-
-    // 42 -5 12 21 5 24 -5              ---> expected: 5, got: -5
-    // -5 -4 -2 12 -40 4 2 18 11 5 -2   ---> expected: 2, got: -2
 }
